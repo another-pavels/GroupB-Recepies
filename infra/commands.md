bb010c77640098da119d821e2cff1f0ac4ef86d8
Pavels Marhels <pavels@lv.lv>
2023-06-06 22:39:43 +0300

    ignore terraform files + update init scripts

--- infra/commands.md
+++ infra/commands.md
@@ -1,2 +1,4 @@
 ### time before shutdown 
 `date --date @$(head -1 /run/systemd/shutdown/scheduled |cut -c6-15)`
+
+
--- infra/init.sh
+++ infra/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e 
 curl -k https://freedns.afraid.org/dynamic/update.php\?SWJHSHBtS3lxRHp4dU53QkduQ2c6MjE1OTcwMDA\= 
+sudo echo "@reboot sudo shutdown -P +180" | crontab -
 sudo apt update && \
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
@@ -10,6 +11,5 @@ sudo apt update && \
 apt-cache policy docker-ce && \
 sudo apt install docker-ce docker-compose net-tools ansible -y
 curl -s -o /opt/ansible-batch.yml "https://raw.githubusercontent.com/another-pavels/GroupB-Recepies/main/infra/ansible-batch.yml" 
-echo "@reboot sudo shutdown -P +180" | crontab
 ansible-playbook /opt/ansible-batch.yml 
 #docker-compose -f /opt/webserver-compose up -d
