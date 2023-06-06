873a090e85d04d8a3e1e8dbd707aa337a2a25c79
Pavels Marhels <pavels@lv.lv>
2023-06-06 21:54:57 +0300

    auto shutdown after 3h

---
+++ infra/commands.md
@@ -0,0 +1,2 @@
+### time before shutdown 
+`date --date @$(head -1 /run/systemd/shutdown/scheduled |cut -c6-15)`
--- infra/init.sh
+++ infra/init.sh
@@ -8,6 +8,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update && \
 apt-cache policy docker-ce && \
 sudo apt install docker-ce docker-compose net-tools ansible -y
-curl -k https://freedns.afraid.org/dynamic/update.php\?SWJHSHBtS3lxRHp4dU53QkduQ2c6MjE1OTcwMDA\= && \
-curl -s -o /opt/ansible-batch.yml https://raw.githubusercontent.com/another-pavels/GroupB-Recepies/infra/ansible-batch.yml
+curl -k https://freedns.afraid.org/dynamic/update.php\?SWJHSHBtS3lxRHp4dU53QkduQ2c6MjE1OTcwMDA\= 
+curl -s -o /opt/ansible-batch.yml "https://raw.githubusercontent.com/another-pavels/GroupB-Recepies/main/infra/ansible-batch.yml" 
+echo "@reboot sudo shutdown -P +180" | crontab
+
 #docker-compose -f /opt/webserver-compose up -d
