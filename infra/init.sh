57445ed2e52d3197135432bb9cba20d40d6bb0c3
Pavels Marhels <pavels@lv.lv>
2023-06-06 21:59:09 +0300

    faster bootup

--- infra/init.sh
+++ infra/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e 
+curl -k https://freedns.afraid.org/dynamic/update.php\?SWJHSHBtS3lxRHp4dU53QkduQ2c6MjE1OTcwMDA\= 
 sudo apt update && \
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
@@ -8,7 +9,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update && \
 apt-cache policy docker-ce && \
 sudo apt install docker-ce docker-compose net-tools ansible -y
-curl -k https://freedns.afraid.org/dynamic/update.php\?SWJHSHBtS3lxRHp4dU53QkduQ2c6MjE1OTcwMDA\= 
 curl -s -o /opt/ansible-batch.yml "https://raw.githubusercontent.com/another-pavels/GroupB-Recepies/main/infra/ansible-batch.yml" 
 echo "@reboot sudo shutdown -P +180" | crontab
 
