f1422a3b3c106f573ae45c77b273bd4e9bb2ac12
Aigars Pluģis <plugis4@gmail.com>
2023-06-22 05:20:52 +0000

    Reverted changes to HTTP 302

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertContains($response->getStatusCode(), array(200,302));
+        $response->assertStatus(302);
     }
 }
